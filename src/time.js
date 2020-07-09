abc6410026426fef0843cc57e03c2bcb9585f1f2
Jonathan Phillips <jonathan.phillips@coxautoinc.com>
2020-07-09 13:50:55 -0600

    added time utils

---
+++ src/time.js
@@ -0,0 +1,29 @@
+import { cond, seqn } from './functional.js';
+
+const Time = {
+    Day: Date.UTC(1970,0,2),
+    Hour: Date.UTC(1970,0,1, 1)
+};
+
+const toPartition = jsd => (
+    typeof jsd === 'object' && jsd.getUTCFullYear && !isNaN(jsd.getUTCFullYear())
+    ? {
+        year: `${jsd.getUTCFullYear()}`.padStart(4, '0'),
+        month: `${jsd.getUTCMonth() + 1}`.padStart(2, '0'),
+        day: `${jsd.getUTCDate()}`.padStart(2, '0'),
+        hour: `${jsd.getUTCHours()}`.padStart(2, '0')
+    }
+    : { year: '', month: '', day: '', hour: '' }
+);
+
+
+const fromPartition = (year = '1970', month = '01', day = '01', hour = '00') => (
+    new Date(Date.UTC(year,month - 1,day,hour))
+);
+
+
+
+const plusDays = (jsd, n) => new Date(jsd.getTime() + (Time.Day * n));
+const minusDays = (jsd, n) => plusDays(jsd, -n);
+const plusHours = (jsd, n) => new Date(jsd.getTime() + (Time.Hour * n));
+const minusHours = (jsd, n) => plusHours(jsd, -n);
